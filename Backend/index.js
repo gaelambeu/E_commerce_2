7abe0dc3fd995fc4fb477cd18ad6c8ad198b88e5
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-24 00:24:23 +0000

    few

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,9 @@ const app = express()
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000;
 
+app.use('/', (req, res) => {
+    res.send("Hello child of God")
+});
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
 });
