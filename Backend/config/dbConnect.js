ad2751c0e54ca2b5ad90da72b081c5b228555076
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-26 22:40:14 +0000

    few

--- Backend/config/dbConnect.js
+++ Backend/config/dbConnect.js
@@ -3,8 +3,10 @@ const { default: mongoose } = require("mongoose")
 const dbConnecct = () => {
     try {
         const conn = mongoose.connect('')
-        console.log("Databse connected");
+        console.log("Databse connected successfully");
     } catch (error) {
         console.log("Databse error");
     }
 }
+
+module.exports= dbConnecct;
