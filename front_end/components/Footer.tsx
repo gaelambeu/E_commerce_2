6803185b553c09c09306a4096f952a7ca0cd3463
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-04 17:58:42 +0000

    short

--- front_end/components/Footer.tsx
+++ front_end/components/Footer.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import { footerLinks
+ } from "@/constants"
+
 const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
@@ -12,7 +15,9 @@ const Footer = () => {
               All rights reserved & copy;
             </p>
          </div>
-         
+         <div className="footer__links">
+
+         </div>
       </div>
     </footer>
   )
