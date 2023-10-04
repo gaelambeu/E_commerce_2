0d75fe65758dc7a414ba12a8fa260f06aa6e4ae3
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-04 17:21:58 +0000

    shoert

--- front_end/components/Navbar.tsx
+++ front_end/components/Navbar.tsx
@@ -7,9 +7,13 @@ const Navbar = () => {
   return (
     <header className='w-full absolute z-10'>
         <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
-            <link href='/' className='flex justify-center items-center'>
-                
-            </link>
+            <Link href='/' className='flex justify-center items-center'>
+                <Image src="/logo.svg" alt='Car Hub logo' width={118} height={18} className='objet-contain' />
+            </Link>
+
+            <CustomButton 
+              
+            />
         </nav>
     </header>
   )
