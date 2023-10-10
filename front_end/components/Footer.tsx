802a92d13a94bb778d0de0237d7c8823ae6c95cc
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-10 22:55:46 +0000

    continue make footer

--- front_end/components/Footer.tsx
+++ front_end/components/Footer.tsx
@@ -27,13 +27,20 @@ const Footer = () => {
               </div>
             ))}
          </div>
-         <div className="flex justify-between items-center flex-wrap mt-10 border-t border-green-100 sm:px-16 px-6 py-10">
-           <div className="footer__copyrights-links">
-              <Link href="/" >
-              </Link>
-           </div>
-         </div>
-      </div>
+        </div>
+
+        <div className="flex justify-between items-center flex-wrap mt-10 border-t border-green-100 sm:px-16 px-6 py-10">
+          <p>@2023 CarHub. All Rights Reserved</p>
+          <div className="footer__copyrights-links">
+            <Link href="/" className="text-gray-500">
+              Privacy Policy
+            </Link>
+            <Link href="/" className="text-gray-500">
+              Terms of Use
+            </Link>
+          </div>
+        </div>
+     
     </footer>
   )
 }
