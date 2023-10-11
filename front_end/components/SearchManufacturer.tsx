6b94f70d8d9994c1857b380dd4bf9802146f83c5
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-11 00:36:44 +0000

    update

--- front_end/components/SearchManufacturer.tsx
+++ front_end/components/SearchManufacturer.tsx
@@ -4,8 +4,11 @@ import Image from 'next/image'
 
 import { Combobox, Transition } from '@headlessui/react'
 import { SearchManufacturerProps } from '@/types'
+import { useState } from 'react'
 
 const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturerProps) => {
+
+    const [query, setQuery] = useState('')
   return (
     <div className="search-manufacturer">
         <Combobox>
@@ -13,6 +16,13 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
                 <Combobox.Button className="absolute top-[14px]">
                     <Image src="/car-logo.svg" width={20} height={20} className="ml-4" alt="Car Logo" />
                 </Combobox.Button>
+
+                <Combobox.Input 
+                className="search-manufacturer__input"
+                placeholder="Volkswagen"
+                displayValue={(manufacturer: string) => manufacturer} 
+                onChange={(e) => setQuery(e.target.value)}
+                />
             </div>
         </Combobox>
     </div>
