ebdd606494706cbc17a1a145ecb8fa6eedc942af
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-11 00:17:33 +0000

    Update SearchManufacturer

--- front_end/components/SearchManufacturer.tsx
+++ front_end/components/SearchManufacturer.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Image from 'next/image'
+
 import { Combobox, Transition } from '@headlessui/react'
 import { SearchManufacturerProps } from '@/types'
 
@@ -8,8 +10,8 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
     <div className="search-manufacturer">
         <Combobox>
             <div className="relative w-full">
-                <Combobox.Button className="">
-                    
+                <Combobox.Button className="absolute top-[14px]">
+                    <Image src="/car-logo.svg" width={20} height={20} className="ml-4" alt="Car Logo" />
                 </Combobox.Button>
             </div>
         </Combobox>
