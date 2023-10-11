5f73bdde2298e8b85d986b472cc7cf108b77295e
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-11 00:43:10 +0000

    transition

--- front_end/components/SearchManufacturer.tsx
+++ front_end/components/SearchManufacturer.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 import { Combobox, Transition } from '@headlessui/react'
 import { SearchManufacturerProps } from '@/types'
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 
 const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturerProps) => {
 
@@ -23,6 +23,16 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
                 displayValue={(manufacturer: string) => manufacturer} 
                 onChange={(e) => setQuery(e.target.value)}
                 />
+
+                <Transition 
+                    as={Fragment} 
+                    leave="transition ease-in duration-100" 
+                    leaveFrom="opacity-100" 
+                    leaveTo="opacity-0" 
+                    afterLeave={() => setQuery('')} 
+                >
+
+                </Transition>
             </div>
         </Combobox>
     </div>
