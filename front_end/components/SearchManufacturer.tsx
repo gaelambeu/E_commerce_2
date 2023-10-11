d30cb4f2cbb34d614f6e6ec225488661a3508f4f
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-11 00:00:14 +0000

    index styles

--- front_end/components/SearchManufacturer.tsx
+++ front_end/components/SearchManufacturer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SearchManufacturer = () => {
+const SearchManufacturer = ({manufacturer, setManufacturer} :) => {
   return (
     <div>SearchManufacturer</div>
   )
